68c7c045b7a21a933c2e9dc5da8879e97a17b18c
electrolyte-orb <electrolyte.spdf@outlook.com>
2021-10-10 16:56:54 +0530

    added firebase

---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: 'AIzaSyC0fw1J6ZlbjNk8LMY_pt8tU1_9gxwcd-U',
+  authDomain: 'firaskled-a.firebaseapp.com',
+  projectId: 'firaskled-a',
+  storageBucket: 'firaskled-a.appspot.com',
+  messagingSenderId: '42711363792',
+  appId: '1:42711363792:web:c46e794a6a0c3f6bb6aed5',
+};
